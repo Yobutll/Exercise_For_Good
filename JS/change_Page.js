c6593162ba2fe6b4ea50f9ce5f7dd1f7b1cc79c7
Yobutll <108317072+Yobutll@users.noreply.github.com>
2023-10-09 20:38:51 +0700

    09/10

--- JS/change_Page.js
+++ JS/change_Page.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", function() {
     let ExLink = document.getElementById("Ex")
     let HomeLink = document.getElementById("Home")
     let footballLink = document.getElementById("football");
+    let fitnessLink = document.getElementById("fitness");
+    let parkLink = document.getElementById("park");
+    let basLink = document.getElementById("bas");
 
     aboutLink.addEventListener("click", function(event) {
         event.preventDefault();
@@ -28,6 +31,21 @@ document.addEventListener("DOMContentLoaded", function() {
     footballLink.addEventListener("click", function(event) {
         event.preventDefault();
         window.location.href = "football.html";
+    });
+
+    fitnessLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        window.location.href = "fitness.html";
+    });
+
+    parkLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        window.location.href = "park.html";
+    });
+
+    basLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        window.location.href = "basketball.html";
     });
 
 });
--- copilot-test.js
+++
@@ -1,3 +0,0 @@
-function calculateDayBetweenDates(Begin, end) {
-    
-}
